0561a8930bc97192ee812e821f59bbf687fa3710
Estefano Chacin <estefanochacin@wposs.com>
2023-08-01 17:22:41 -0500

    lllllllllllllll

--- Capa_Entidad/Usuario.cs
+++ Capa_Entidad/Usuario.cs
@@ -8,7 +8,7 @@ namespace Capa_Entidad
         public int? Id { get; set; }
         public string? Nombre { get; set; }
         public string? Contrasena { get; set; }
-        public string? Rol { get; set; }
+        public string? Rolllllllllllllll { get; set; }
         public string? pruebaDesdeRama { get; set; }
         public string? otraPrueba { get; set; }
         public string? otraaaa { get; set; }
